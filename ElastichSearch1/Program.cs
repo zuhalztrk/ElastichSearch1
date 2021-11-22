6d75c471aa2c96ba8fc95e5ac7f95d12561703b2
suhallztrk@gmail.com <suhallztrk@gmail.com>
2021-11-22 15:50:49 +0300

    Add project files.

---
+++ ElastichSearch1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Elasticsearch.Net;
+using System;
+
+namespace ElastichSearch1
+{
+    class Program
+    {
+        public class Customer
+        {
+          static async System.Threading.Tasks.Task Main(string[] args)
+            {
+                var setting = new ConnectionConfiguration(new Uri("http://localhost:9200")).RequestTimeout(TimeSpan.FromMinutes(2));
+
+                var client = new ElasticLowLevelClient(setting);
+                var car = new car("Ford", 20000);
+                var response = await client.IndexAsync<StringResponse>("car", "1", PostData.Serializable(car));
+                string responseString = response.Body;
+                Console.WriteLine(response);
+
+
+            }
+        }
+        
+    }
+}
---
+++ ElastichSearch1/car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElastichSearch1
+{
+    public class car
+    {
+
+        public car(string make,int price )
+        {
+            make = make;
+            price = price;
+        }
+        public string Make { get; set; }
+        public int price { get; set; }
+    }
+}
